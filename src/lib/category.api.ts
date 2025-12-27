cbca9e4b0e0549972b5e95fd717cd4e705ca5122
Miguel Cobo <sonegs@hotmail.com>
2025-12-27 18:30:57 +0100

    refactor: rename list api to category api

--- src/lib/category.api.ts
+++ src/lib/category.api.ts
@@ -1,10 +1,10 @@
 import { Endpoints } from "./utils/constants/endpoints";
 
 export async function getCategories() {
-  const res = await fetch(`${Endpoints.MERCADONA}categories/`);
-  if (!res.ok) {
+  const categories = await fetch(`${Endpoints.MERCADONA}categories/`);
+  if (!categories.ok) {
     throw new Error('Failed to fetch posts');
   }
 
-  return res.json();
+  return categories.json();
 }
