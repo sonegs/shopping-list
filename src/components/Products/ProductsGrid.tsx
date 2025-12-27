f582bebb3301bcd23d1b6c539ab17e04418914a6
Miguel Cobo <sonegs@hotmail.com>
2025-12-27 19:22:15 +0100

    chore(products): add todo

--- src/components/Products/ProductsGrid.tsx
+++ src/components/Products/ProductsGrid.tsx
@@ -1,6 +1,7 @@
 
 export default async function ProductsGrid() {
   // TODO: Add product mapper by ProductsCard
+  // TODO: Virtualize products to improve performance
   return (
     <>Products Grid</>
   )    
