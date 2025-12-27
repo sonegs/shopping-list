bb7e5a76d911c87844de18aad6c07aac4121c372
Miguel Cobo <sonegs@hotmail.com>
2025-12-27 18:49:52 +0100

    refactor(api): add types

--- src/lib/category.api.ts
+++ src/lib/category.api.ts
@@ -1,6 +1,26 @@
 import { Endpoints } from "./utils/constants/endpoints";
 
-export async function getCategories() {
+type CategoriesResponse = {
+  count: number,
+  next: unknown,
+  previous: unknown,
+  results: CategoriesResult[],
+}
+
+type CategoriesResult = {
+  id: number,
+  order: number,
+  is_extended: boolean,
+  name: string,
+  categories: Category[],
+}
+
+type Category = CategoriesResult & {
+  published: boolean,
+  layout: number,
+}
+
+export async function getCategories(): Promise<CategoriesResponse> {
   const categories = await fetch(`${Endpoints.MERCADONA}categories/`);
   if (!categories.ok) {
     throw new Error('Failed to fetch posts');
